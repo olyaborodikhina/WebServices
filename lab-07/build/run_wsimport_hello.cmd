2967e9e061d1bbd1f0346a8afb23a5b364a7857e
omsk17 <olyaborodikhina>
2016-12-16 12:35:38 +0700

    lab 7 - not done

---
+++ lab-07/build/run_wsimport_hello.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+call ..\..\env.cmd
+
+set WSDL_LOCATION=..\src\main\resources\hello.wsdl
+set OUTPUT_LOCATION=.\output\hello
+
+:CHK_WSDL
+if exist %WSDL_LOCATION% goto CHK_OUTPUT
+echo WSDL not found (%WSDL_LOCATION%)
+goto EDN
+
+:CHK_OUTPUT
+if exist %OUTPUT_LOCATION% goto RUN
+mkdir %OUTPUT_LOCATION%
+
+:RUN
+%JAVA_HOME%\bin\wsimport.exe -keep -d %OUTPUT_LOCATION% %WSDL_LOCATION%
+
+:EDN
