0ec910f637e42ab9d8786ff42f5e88018a2400af
omsk17 <olyaborodikhina>
2016-12-19 12:28:35 +0700

    lab 8 - done ex1 and ex2, but not done ex3(19.12.16)

--- lab-08/src/main/java/ws/echo/EchoProviderImpl.java
+++ lab-08/src/main/java/ws/echo/EchoProviderImpl.java
@@ -1,16 +1,37 @@
 package ws.echo;
 
+import com.sun.xml.internal.messaging.saaj.soap.ver1_1.SOAPPart1_1Impl;
+import com.sun.xml.internal.messaging.saaj.soap.ver1_2.SOAPFactory1_2Impl;
+import com.sun.xml.internal.messaging.saaj.soap.ver1_2.SOAPMessageFactory1_2Impl;
+import com.sun.xml.internal.ws.message.jaxb.JAXBMessage;
+import org.w3c.dom.NodeList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stax.StAXSource;
 import javax.xml.ws.Provider;
+import javax.xml.ws.Service;
+import javax.xml.ws.ServiceMode;
+import javax.xml.ws.WebServiceProvider;
 
 /**
  * @author Anton German &lt;AGerman@luxoft.com&gt;
  * @version 1.0 08.01.14
  */
-public class EchoProviderImpl{
+@WebServiceProvider(serviceName = "EchoService", targetNamespace = "http://echo.ws/")
+@ServiceMode(Service.Mode.MESSAGE)
+public class EchoProviderImpl implements Provider<SOAPMessage> {
 
     /**
      * Request class
@@ -19,9 +40,9 @@ public class EchoProviderImpl{
     @XmlAccessorType(XmlAccessType.FIELD)
     static class EchoRequest {
         @XmlElement(name = "arg0")
-        protected String message;
+        public String message;
         @XmlElement(name = "arg1")
-        protected int count;
+        public int count;
     }
 
     /**
@@ -34,5 +55,25 @@ public class EchoProviderImpl{
         protected String message;
     }
 
+    @Override
+    public SOAPMessage invoke(SOAPMessage request) {
+        try {
+            JAXBContext context = JAXBContext.newInstance(EchoRequest.class, EchoResponse.class);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            NodeList params = request.getSOAPBody().getChildNodes().item(0).getChildNodes();
+            String message = params.item(0).getNodeValue();
+            int count = Integer.parseInt(params.item(1).getNodeValue());
 
+
+//            final StringBuilder resultBuilder = new StringBuilder();
+//            for(int i = 0; i < echoRequest.count; i++){
+//                resultBuilder.append(echoRequest.message);
+//            }
+//            EchoResponse response = new EchoResponse();
+//            response.message = resultBuilder.toString();
+            return SOAPMessageFactory1_2Impl.newInstance().createMessage();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
